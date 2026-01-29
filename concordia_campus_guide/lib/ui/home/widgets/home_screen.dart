c2b7c8b5bc797c674841ecf1962b6607d9a22415
Viresh Patel <viresh.p2003@gmail.com>
2026-01-29 17:17:46 -0500

    Add and document google maps API integration

--- concordia_campus_guide/lib/ui/home/widgets/home_screen.dart
+++ concordia_campus_guide/lib/ui/home/widgets/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:concordia_campus_guide/ui/core/ui/campus_app_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,18 +10,13 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: const CampusAppBar(),
 
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            Text(
-              'Welcome to Concordia Campus Guide',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 16),
-            Text('Your campus navigation app', style: TextStyle(fontSize: 16)),
-          ],
+      body: const GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: LatLng(45.4972, -73.5786),
+          zoom: 15,
         ),
+        myLocationButtonEnabled: false,
+        zoomControlsEnabled: false,
       ),
     );
   }
