e33d20c8968283a444c1bb9710cc9ed7a36b15bc
Viresh Patel <viresh.p2003@gmail.com>
2026-01-31 18:35:41 -0500

    Fix flutter analyze issues

--- concordia_campus_guide/lib/ui/core/themes/app_theme.dart
+++ concordia_campus_guide/lib/ui/core/themes/app_theme.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  static const Color concordiaMaroon = const Color(0xFF912338);
+  static const Color concordiaMaroon = Color(0xFF912338);
 
   static ThemeData mainTheme = ThemeData(
     appBarTheme: AppBarTheme(backgroundColor: concordiaMaroon),
--- concordia_campus_guide/lib/ui/home/widgets/home_screen.dart
+++ concordia_campus_guide/lib/ui/home/widgets/home_screen.dart
@@ -12,8 +12,13 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  static const Coordinate sgw = Coordinate(latitude: 45.4972, longitude: -73.5786);
-  static const Coordinate loyola = Coordinate(latitude: 45.45823348665408, longitude: -73.64067095332564);
+  static const Coordinate sgw = Coordinate(
+    latitude: 45.4972,
+    longitude: -73.5786,
+  );
+
+  // Coordinates for loyola, uncomment if needed
+  // static const Coordinate loyola = Coordinate(latitude: 45.45823348665408, longitude: -73.64067095332564);
 
   @override
   Widget build(BuildContext context) {
@@ -21,10 +26,7 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: const CampusAppBar(),
 
       body: GoogleMap(
-        initialCameraPosition: CameraPosition(
-          target: sgw.toLatLng(),
-          zoom: 15,
-        ),
+        initialCameraPosition: CameraPosition(target: sgw.toLatLng(), zoom: 15),
         myLocationButtonEnabled: false,
         zoomControlsEnabled: false,
       ),
