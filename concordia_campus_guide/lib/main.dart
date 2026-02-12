131f114280b2d34071bb4351b5b151b252b66d17
kxr <kxrury@gmail.com>
2026-02-11 20:01:26 -0500

    init firebase

--- concordia_campus_guide/lib/main.dart
+++ concordia_campus_guide/lib/main.dart
@@ -5,8 +5,12 @@ import "package:concordia_campus_guide/ui/home/widgets/home_screen.dart";
 import "package:flutter/material.dart";
 import "package:provider/provider.dart";
 import "package:concordia_campus_guide/ui/core/themes/app_theme.dart";
+import "package:firebase_core/firebase_core.dart";
+import "firebase_options.dart";
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -16,18 +20,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(final BuildContext context) {
     return MultiProvider(
-      providers: [ChangeNotifierProvider(
-        create: (_) => HomeViewModel(
-          mapInteractor: MapDataInteractor(
-            buildingRepo: BuildingRepository()
-          )
-        )
-      )],
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => HomeViewModel(
+            mapInteractor: MapDataInteractor(
+              buildingRepo: BuildingRepository(),
+            ),
+          ),
+        ),
+      ],
       child: MaterialApp(
         title: "Concordia Campus Guide",
         theme: AppTheme.mainTheme,
         home: const HomeScreen(),
-        debugShowCheckedModeBanner: false
+        debugShowCheckedModeBanner: false,
       ),
     );
   }
