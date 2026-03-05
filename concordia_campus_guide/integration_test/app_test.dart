862832ebfe3ac85b1e6f8d2d0f14d7ce7c185b34
AndriiBranytskyi <andrii.branytskyi@mail.concordia.ca>
2026-03-04 21:27:14 -0500

    Entry point for patrol testing plus e2e test for us 11 12 13

---
+++ concordia_campus_guide/integration_test/app_test.dart
@@ -0,0 +1,16 @@
+import "package:concordia_campus_guide/main.dart" as app;
+import "package:firebase_core/firebase_core.dart";
+import "package:flutter_test/flutter_test.dart";
+import "package:patrol/patrol.dart";
+
+void main() {
+  patrolTest(
+    "App boots",
+        (final $) async {
+      TestWidgetsFlutterBinding.ensureInitialized();
+      await Firebase.initializeApp();
+      app.main();
+      await $.pumpAndSettle();
+    },
+  );
+}
