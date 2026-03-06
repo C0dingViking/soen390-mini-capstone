b72cf6fee3149e098a776a54bf5ac761d3bf3659
AndriiBranytskyi <andrii.branytskyi@mail.concordia.ca>
2026-03-05 23:47:52 -0500

    Patrol for us 14 and 15

---
+++ concordia_campus_guide/patrol_test/patrol_15_test.dart
@@ -0,0 +1,37 @@
+import "package:concordia_campus_guide/main.dart" as app;
+import "package:flutter_test/flutter_test.dart";
+import "package:google_maps_flutter/google_maps_flutter.dart";
+import "package:patrol/patrol.dart";
+
+void main() {
+  patrolTest(
+    "US 1.5 Show Building info",
+        (final $) async {
+      $.log("STEP 1: Launching the app...");
+      app.main();
+      await $.pumpAndSettle();
+
+      $.log("STEP 2: Handling location permission popup...");
+      await $.platformAutomator.tap(
+        Selector(text: "Only this time"),
+        timeout: const Duration(seconds: 10),
+      );
+
+      await $.pumpAndSettle();
+      await $.pump(const Duration(seconds: 6));
+
+      final mapFinder = find.byType(GoogleMap);
+
+      $.log("STEP 3: Tapping the Hall building");
+      final center = $.tester.getCenter(mapFinder);
+      final tapPoint = center + const Offset(-5, 5);
+      await $.tester.tapAt(tapPoint);
+      await $.pumpAndSettle();
+
+      $.log("STEP 4: Waiting for building info to appear...");
+      await $.tester.pump(const Duration(seconds: 5));
+
+      $.log("TEST COMPLETE");
+    },
+  );
+}
