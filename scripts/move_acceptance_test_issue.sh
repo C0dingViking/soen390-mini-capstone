9378819687e651b96e5b0ef1402e4b669c01d943
C0dingViking <william.ross.lpp.83@gmail.com>
2026-03-12 14:02:29 -0400

    completed graphql queries and bash script

--- scripts/move_acceptance_test_issue.sh
+++ scripts/move_acceptance_test_issue.sh
@@ -4,10 +4,6 @@ set -u -o pipefail
 FAILED=false
 ERROR_MSG=""
 
-CYAN='\033[0:36m'
-
-NC='\033[0m' # No Color
-
 OWNER="$1"
 REPO="$2"
 ISSUE_NUMBER=$3
@@ -16,15 +12,18 @@ PROJECT_ID="PVT_kwHOBh_Zsc4BMeio"
 STATUS_FIELD_ID="PVTSSF_lAHOBh_Zsc4BMeiozg7v4es"
 AWAITING_PO_APPROVAL_OPTION_ID="b79f21be"
 
-echo -e "${CYAN}INFO: Moving issue #${ISSUE_NUMBER} to 'Awaiting PO Approval' column${NC}"
+echo -e "INFO: Moving issue #${ISSUE_NUMBER} to 'Awaiting PO Approval' column"
 
 echo "Getting current PR's associated issue's subissues..."
 
-RESPONSE=$(gh api graphql -f query="$(cat ../graphql/get_subissue.graphql)" -F parent_number=${ISSUE_NUMBER} -F owner="${OWNER}" -F repo="${REPO}")
+RESPONSE=$(gh api graphql \
+  -f query="$(cat ../queries/get_subissue.graphql)" \
+  -F issueNb=${ISSUE_NUMBER} -F owner="${OWNER}" \
+  -F repo="${REPO}")
 
 SUB_ISSUE_COUNT=$(echo "$RESPONSE" | jq '.data.repository.issue.subIssues.nodes | length')
 
-GRAPHQL_ERROS=$(echo "$RESPONSE" | jq '.errors')
+GRAPHQL_ERRORS=$(echo "$RESPONSE" | jq '.errors')
 if [ "$GRAPHQL_ERRORS" != "null" ]; then
     ERROR_MSG=$(echo "$GRAPHQL_ERRORS" | jq -r '.[0].message')
     echo "::error title=GraphQL Error::$ERROR_MSG"
@@ -38,13 +37,31 @@ fi
 
 echo "Found ${SUB_ISSUE_COUNT} subissue(s). Getting the Acceptance Test subissue (if any) and moving it..."
 
-AT_SUB_ISSUES=$(echo "$RESPONSE" | jq '
+AT_SUB_ISSUES=$(jq '
   .data.repository.issue.subIssues.nodes
-  | map(
-      select(
-        (.labels.nodes // [] | any(.name == "acceptance-test"))
-      )
-    )
-')
+  | map(select(.labels.nodes // [] | any(.name == "acceptance test")))
+' <<< "$RESPONSE")
 
-AT_SUB_ISSUE_COUNT=$(echo "$AT_SUB_ISSUES" | jq 'length')
+NB_AT_SUB_ISSUES=$(jq 'length' <<< "$AT_SUB_ISSUES")
+
+if [[ $NB_AT_SUB_ISSUES -eq 0 ]]; then
+  echo "::warning title=No Acceptance Test subissue found::No subissue with 'acceptance test' label found for issue #${ISSUE_NUMBER}. Exiting."
+  exit 0 # No AT subissue to move, but not an error condition so exit with success
+fi
+
+AT_PROJECT_CARD_ID=$(jq -r '.[0].projectItems.nodes[0].id' <<< "$AT_SUB_ISSUES")
+
+RESPONSE=$(gh api graphql \
+  -f query="$(cat ../queries/mutate_project_card.graphql)" \
+  -F projectId="${PROJECT_ID}" -F itemId="${AT_PROJECT_CARD_ID}" \
+  -F fieldId="${STATUS_FIELD_ID}" \
+  -F newOptionId="${AWAITING_PO_APPROVAL_OPTION_ID}")
+
+ERRORS=$(jq -r '.errors // empty' <<< "$RESPONSE")
+
+if [[ -n "$ERRORS" ]]; then
+  echo "::warning title=Project Card Mutation Failed::An error occurred while updating the project card: $(jq -c '.' <<< "$ERRORS")"
+  exit 1
+fi
+
+echo "✅ Project card updated successfully for item ID ${AT_PROJECT_CARD_ID}"
